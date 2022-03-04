32469056ee38ffeaf5f28344238d0818ac40bcff
xproavo <62142457+xproavo@users.noreply.github.com>
2022-03-04 18:12:44 +0100

    player move

--- Assets/Scripts/GamePlay/Player/InputManager.cs
+++ Assets/Scripts/GamePlay/Player/InputManager.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
 
+    public LayerMask GroundLayer;
 
+
+    public float SpeedMove = 5;
+
+
+    public bool IsGrounded = false;
 
     private bool _controlEnabled = true;
     private bool _jump, _stopJump;
@@ -14,8 +20,11 @@ public class InputManager : MonoBehaviour
 
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
+    private Collider2D _collider2d;
+    private Rigidbody2D _body2d;
 
     private JumpState _jumpState = JumpState.Grounded;
+
 
 
     // Start is called before the first frame update
@@ -23,6 +32,8 @@ public class InputManager : MonoBehaviour
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        _collider2d = GetComponent<Collider2D>();
+        _body2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -37,14 +48,65 @@ public class InputManager : MonoBehaviour
             else if (Input.GetButtonDown("Jump"))
                 _stopJump = true;
         }
-       
+        Movement();
     }
+
+    void UpdateJumpState()
+    {
+        _jump = false;
+        switch (_jumpState)
+        {
+            case JumpState.PrepareToJump:
+                _jumpState = JumpState.Jumping;
+                _jump = true;
+                _stopJump = false;
+                break;
+
+            case JumpState.Jumping:
+                if (!IsGrounded)
+                    _jumpState = JumpState.InFlight;
+                break;
+
+            case JumpState.InFlight:
+                if (IsGrounded)
+                    _jumpState = JumpState.Landed;
+                break;
+
+            case JumpState.Landed:
+                _jumpState = JumpState.Grounded;
+                break;
+        }
+    }
+
 
     private void Movement()
     {
+        _body2d.velocity = _move * SpeedMove;
 
+        if (_move.x > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_move.x < -0.01f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+
+        Vector3 rayCastDir = Vector3.down;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayCastDir, 0.5f, GroundLayer.value);
+        IsGrounded = hit.collider != null;
+
+
+        _animator.SetFloat("velocityX", Mathf.Abs(_body2d.velocity.x));
+        _animator.SetBool("grounded", IsGrounded);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, _body2d.velocity);
+    }
 
     public enum JumpState
     {
