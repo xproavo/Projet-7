e3a36105439dceb6e7e402b704559e118bef4e27
xproavo <62142457+xproavo@users.noreply.github.com>
2022-03-16 17:29:30 +0100

    .

--- Assets/Scripts/GamePlay/Projectile/Arrow.cs
+++ Assets/Scripts/GamePlay/Projectile/Arrow.cs
@@ -22,17 +22,25 @@ public class Arrow : MonoBehaviour
         this.gameObject.GetComponent<Rigidbody2D>().AddForce(vec2 * force);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-    
         if (collision.transform.tag == "Player" && CanTouchPlayer)
         {
             collision.gameObject.GetComponent<StateManager>().Attack(Damage);
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(_dirToMove.x * HitForceX, HitForceY));
             Destroy(gameObject);
+        }else if(collision.transform.tag == "Enemy")
+        {
+            return;
         }
-    
+        HitForceY = 0;
+        HitForceX = 0;
+        Damage = 0;
+        Destroy(gameObject, 1f);
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.transform.tag == "Enemy")
         {
             if (collision.GetComponent<StateManager>().Death)
@@ -42,10 +50,6 @@ public class Arrow : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(_dirToMove.x * HitForceX, HitForceY));
             Destroy(gameObject);
         }
-        HitForceY = 0;
-        HitForceX = 0;
-        Damage = 0;
-        Destroy(gameObject, 0.4f);
     }
 
     private void UnOverlapPlayer()
