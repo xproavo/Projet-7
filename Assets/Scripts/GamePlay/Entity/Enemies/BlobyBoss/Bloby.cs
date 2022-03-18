f2e4c2de83bba70d7ae9dd89a320dccc6d879aba
xproavo <62142457+xproavo@users.noreply.github.com>
2022-03-18 05:32:24 +0100

    boss finish

--- Assets/Scripts/GamePlay/Entity/Enemies/BlobyBoss/Bloby.cs
+++ Assets/Scripts/GamePlay/Entity/Enemies/BlobyBoss/Bloby.cs
@@ -9,6 +9,7 @@ public class Bloby : MonoBehaviour
 
     public Vector2 HitForce;
     public GameObject[] Projectil;
+    public float MaxAttackRockInstant = 5;
 
     [Range(0f, 100f)]
     public float RageJauge;
@@ -83,22 +84,26 @@ public class Bloby : MonoBehaviour
     {
         if (Projectil.Length > 0)
         {
-            int randProj = Random.Range(0, Projectil.Length);
+            for (int i = 0; i < Random.Range(0, MaxAttackRockInstant); i++)
+            {
+                int randProj = Random.Range(0, Projectil.Length);
 
-            Vector3 pos1 = transform.GetChild(1).transform.position;
-            Vector3 pos2 = transform.GetChild(2).transform.position;
+                Vector3 pos1 = transform.GetChild(1).transform.position;
+                Vector3 pos2 = transform.GetChild(2).transform.position;
 
-            float X = Random.Range(pos1.x, pos2.x);
+                float X = Random.Range(pos1.x, pos2.x);
 
-            GameObject clone = GameObject.Instantiate(Projectil[randProj], new Vector3(X, pos1.y), Quaternion.identity);
-            clone.gameObject.GetComponent<Roc>().Init();
+                GameObject clone = GameObject.Instantiate(Projectil[randProj], new Vector3(X, pos1.y), Quaternion.identity);
+                clone.gameObject.GetComponent<Roc>().Init();
+            }
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Player"))
-        { 
+        if (collision.gameObject.CompareTag("Player"))
+        {
             collision.gameObject.GetComponent<StateManager>().Attack(_stateManager.Damage);
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2((-collision.gameObject.GetComponent<MoveManager>().DirectionSee().x) * HitForce.x, HitForce.y));
         }
